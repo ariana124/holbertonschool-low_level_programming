0b0fcbb9e309687f29e38d13ba02e536be2cb34d
arianab <857@holbertonschool.com>
2019-09-20 22:27:26 +0000

    print spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints number 0-9
+ * Return: always 0
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+
+	while ('0' + c <= '9')
+	{
+		putchar('0' + c);
+		if (!('0' + c == '9'))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
