69816273f1d43309fc558ab4086cacfe05f7211f
arianab <857@holbertonschool.com>
2019-09-20 06:38:58 +0000

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet from a to z, with c incrementing by 1
+ * Return: the lowercase alphabet
+ */
+int main(void)
+{
+	char c;
+	c = 'a';
+	while(c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
