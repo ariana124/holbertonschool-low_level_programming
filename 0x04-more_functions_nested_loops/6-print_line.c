2e45d018363f79bbc61c363d2fd556a64d8d5181
arianab <857@holbertonschool.com>
2019-09-26 20:30:25 +0000

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_line - prints straight line
+ * Return: 0 for success
+ */
+void print_line(int n)
+{
+	int a = 0;
+
+	if (n > 0)
+	{
+		while (a < n)
+		{
+			_putchar ('_');
+			a++;
+		}
+		_putchar ('\n');
+	}
+}
