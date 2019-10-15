f16cb1f340f7b3648bf89f215af4a8102f629e13
arianab <857@holbertonschool.com>
2019-10-15 23:25:17 +0000

    added strlen after ifs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,14 +10,17 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i, j;
-	int len1 = _strlen(s1);
-	int len2 = _strlen(s2);
+	int len1;
+	int len2;
 	char *new;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 
 	new = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 
