5077effae5d0774f58dc0ec2e408efd5f7cb4bc4
arianab <857@holbertonschool.com>
2019-10-16 05:22:51 +0000

    unfinished

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - function that concatenates all arguments of the program
+ * @ac: integer
+ * @av: double pointer
+ * Return: pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	char *cat;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+
+}
