895823a40fa2cd1bacfdc2bf49724abb437f61e3
arianab <857@holbertonschool.com>
2019-10-17 21:18:55 +0000

    deleted sizeof

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	array = malloc((nmemb * size) * sizeof(int));
+	array = malloc(nmemb * size);
 
 	if (array == NULL)
 		return (NULL);
