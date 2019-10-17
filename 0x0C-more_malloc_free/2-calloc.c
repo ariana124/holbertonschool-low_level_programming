0a81573ed29bdeb57ba8d3f756c26dcaf5db04ed
arianab <857@holbertonschool.com>
2019-10-17 21:36:54 +0000

    changed arr to char & added loop

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,8 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *array;
+	unsigned int i;
+	char *array;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -18,6 +19,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (array == NULL)
 		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		array[i] = 0;
 
 	return (array);
 }
