e02b2bde91b50e96eb7159a72254c81deb84a8e2
arianab <857@holbertonschool.com>
2019-10-22 04:39:33 +0000

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dogs
+ * @d: dog pointer
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d);
+}
