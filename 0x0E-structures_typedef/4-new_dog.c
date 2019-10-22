4e42763ce0a330c8ee2f0debd275d961b0a634e7
arianab <857@holbertonschool.com>
2019-10-22 05:22:00 +0000

    added new values to pointers

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,5 +10,64 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *doggo;
+	char *dname, *downer;
 
+	doggo = malloc(sizeof(dog_t)); /* struct dog */
+
+	if (doggo == NULL)
+		return (NULL);
+
+	dname = malloc(_strlen(name) * sizeof(char));
+
+	if (dname == NULL)
+		return (NULL);
+
+	downer = malloc(_strlen(owner) * sizeof(char));
+
+	if (downer == NULL)
+		return (NULL);
+
+	_strcpy(dname, name);
+	_strcpy(downer, owner);
+
+	doggo->name = dname;
+	doggo->age = age;
+	doggo->owner = downer;
+
+	return (doggo);
+}
+
+
+
+/**
+ * _strcpy - copies string to another string
+ * @dest: string 1
+ * @src: string 2
+ * Return: string copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; a <= _strlen(src); a++)
+		dest[a] = src[a];
+	return (dest);
+}
+
+
+/**
+ * _strlen - gives length of a string
+ * @s: string
+ * Return: returns length of string
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	l = 0;
+
+	while (s[l])
+		l++;
+	return (l);
 }
