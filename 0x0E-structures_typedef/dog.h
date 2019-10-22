28a1e76daf19fd6c272a771862a13f60f0e62c4a
arianab <857@holbertonschool.com>
2019-10-22 04:37:43 +0000

    added last prototype & strcpy/len

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,4 +22,12 @@ void print_dog(struct dog *d);
 
 int _putchar(char c);
 
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+char *_strcpy(char *dest, char *src);
+
+int _strlen(char *s);
+
 #endif /* DOG_H */
