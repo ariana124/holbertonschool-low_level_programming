a8642c489d5734a01af320bd0de913be02a221ad
arianab <857@holbertonschool.com>
2019-10-24 21:05:38 +0000

    changed order of exit 99

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,18 +11,18 @@ int main(int argc, char **argv)
 	int a, b;
 	int (*p)(int, int);
 
-	p = get_op_func(argv[2]);
-
-	if (p == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+	p = get_op_func(argv[2]); /* stores function into a pointer */
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
+	}
+
+	if (p == NULL)
+	{
+		printf("Error\n");
+		exit(99);
 	}
 
 	a = atoi(argv[1]);
