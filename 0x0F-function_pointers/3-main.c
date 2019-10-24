bbe85a08856a509ebff600b598b2aaa4b052f432
arianab <857@holbertonschool.com>
2019-10-24 04:44:41 +0000

    added 2 ifs

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,5 +8,16 @@
  */
 int main(int argc, char argv[])
 {
+	if (argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if(argv[2] == '/' || argv[2] == '%' && argv[3] == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
 }
