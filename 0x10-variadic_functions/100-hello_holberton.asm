5a0059d188e0a436063ebb1282ee0a646b2173da
arianab <857@holbertonschool.com>
2019-10-27 18:36:10 +0000

    hello holb in assembly

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,16 @@
+	; Writes "Hello, Holberton" to the system using only systmem calls.
+
+	   global    main
+
+	   section   .text
+main:	   mov       rax, 1
+	   mov       rdi, 1
+	   mov       rsi, holberton
+	   mov       rdx, 17
+	   syscall
+	   mov       rax, 60
+	   xor       rdi, rdi
+	   syscall
+
+	   section   .data
+holberton: db        "Hello, Holberton", 10
