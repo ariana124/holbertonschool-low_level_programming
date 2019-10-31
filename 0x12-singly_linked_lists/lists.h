6bee39ae0fecac40ec30c2118240ffd9855baf74
ariana124 <ariana.bibiano@gmail.com>
2019-10-31 23:14:08 +0000

    added strlen and string.h library

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,8 +23,12 @@ typedef struct list_s
 
 int _putchar(char c);
 
+int _strlen(const char *s);
+
 size_t print_list(const list_t *h);
 
 size_t list_len(const list_t *h);
+
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* LISTS_H */
