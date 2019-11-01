0692315d076660b0c207a5b7ab0004d622a43342
ariana124 <ariana.bibiano@gmail.com>
2019-11-01 02:25:43 +0000

    premain function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * premain - prints the string before the main function
+ *
+ * Return: void
+ */
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
