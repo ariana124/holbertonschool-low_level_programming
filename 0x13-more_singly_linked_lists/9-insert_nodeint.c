ca0d656d9bb911212176c10b540032f262d81600
ariana124 <ariana.bibiano@gmail.com>
2019-11-05 22:13:21 +0000

    moved malloc and added if idx = 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (prev == NULL || *head == NULL)
 		return (NULL);
 
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
 	if (idx != 0)
 	{
 		for (i = 0; i < idx - 1; i++)
@@ -23,10 +28,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 
-	new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
-		return (NULL);
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
 
 	new->n = n;
 
