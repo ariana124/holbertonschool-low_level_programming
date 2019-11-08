71b27623511a4bcd27780cfa60e3c40cfc2e6283
ariana124 <ariana.bibiano@gmail.com>
2019-11-08 18:13:59 +0000

    removed else

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -15,18 +15,15 @@ void print_binary(unsigned long int n)
 		_putchar('0');
 	}
 
-	else
+	for (i = 31; i >= 0; i--)
 	{
-		for (i = 31; i >= 0; i--)
+		binary = n >> i;
+		if (binary & 1)
 		{
-			binary = n >> i;
-			if (binary & 1)
-			{
-				flag = 1;
-				_putchar('1');
-			}
-			else if (flag == 1)
-				_putchar('0');
+			flag = 1;
+			_putchar('1');
 		}
+		else if (flag == 1)
+			_putchar('0');
 	}
 }
