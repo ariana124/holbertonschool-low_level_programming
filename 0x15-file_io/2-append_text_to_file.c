a02db1527fb31b1c9d841c1754bd95bd0aa5c0d8
ariana124 <ariana.bibiano@gmail.com>
2019-11-13 01:46:54 +0000

    append to a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: the name of the file
+ * @text_content: the NULL terminated string to add to the end of the file
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, count;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	file = open(filename, O_APPEND);
+	if (file == -1)
+		return (-1);
+
+	for (count = 0; text_content[count] != '\0'; count++)
+		;
+
+	return (1);
+}
