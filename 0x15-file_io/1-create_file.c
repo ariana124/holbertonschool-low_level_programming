eff05b4e614b42389c6f95b409c45cecb04d08f4
ariana124 <ariana.bibiano@gmail.com>
2019-11-13 00:52:09 +0000

    added truncate

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	file = open(filename, O_CREAT | O_RDWR, 0600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 
