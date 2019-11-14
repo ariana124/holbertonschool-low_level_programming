c0d57d890582911eef4db3d150d985b245449cb4
ariana124 <ariana.bibiano@gmail.com>
2019-11-14 00:48:56 +0000

    added all the checks and error function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,29 +8,57 @@
  */
 int main(int argc, char *argv[])
 {
-	int file;
+	int f1, f2, rc, wc, c1, c2;
 	char buffer[1024];
 
-	if (/*argc is not correct*/)
+	if (argc != 3)
+		error("Usage: cp %s %s\n", "", 97);
+
+	f1 = open(argv[1], O_RDONLY);
+	if (f1 == -1)
+		error("Error: Can't read from file %s\n", argv[1], 98);
+
+	f2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (f2 == -1)
+		error("Error: Can't write to %s\n", argv[2], 99);
+
+	do {
+		rc = read(f1, buffer, 1024);
+		if (rc == -1)
+			error("Error: Can't read from file %s\n", argv[1], 98);
+		wc = write(f2, buffer, rc);
+		if (wc == -1 || wc != rc)
+			error("Error: Can't write to %s\n", argv[2], 99);
+	} while (rc == 1024);
+
+	c1 = close(f1);
+	if (c1 == -1)
 	{
-		dprintf("Usage: cp file_from file_to\n", STDERR_FILENO);
-		exit(97);
-	}
-	if (/*file_from doesn't exist or can't read it*/)
-	{
-		dprintf("Error: Can't read from file NAME_OF_THE_FILE\n",
-			STDERR_FILENO);
-		exit(98);
-	}
-	if (/*cannot create or write to file_to*/)
-	{
-		dprintf("Error: Can't write to NAME_OF_THE_FILE\n",
-			STDERR_FILENO);
-		exit(99);
-	}
-	if (/*cannot close file descriptor*/)
-	{
-		dprintf("Error: Can't close fd FD_VALUE\n", STDERR_FILENO);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c1);
 		exit(100);
 	}
+
+	c2 = close(f2);
+	if (c2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c2);
+		exit(100);
+	}
+
+	return (0);
+}
+
+
+/**
+ * error - function to print error message and exit status
+ * @msg: message to be printed
+ * @file: file name
+ * @status: exit status
+ * Return: void
+ */
+void error(char *msg, char *file, int status)
+{
+	dprintf(STDERR_FILENO, "%s %s\n", msg, file);
+
+	exit(status);
 }
