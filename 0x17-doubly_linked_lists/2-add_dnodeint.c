9bf3bfc7dd1c234b1e19445f1f75623077e1a05e
ariana124 <ariana.bibiano@gmail.com>
2019-12-14 21:32:08 +0000

    added check for if head is null

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,6 +11,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode;
 
+	if (head == NULL)
+		return (NULL);
+
 	newnode = malloc(sizeof(dlistint_t));
 
 	if (newnode == NULL)
