9560e5ca81260d95eb52771111332720ca2658c9
ariana124 <ariana.bibiano@gmail.com>
2020-01-18 00:20:48 +0000

    fixed betty issues

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,6 +3,7 @@
 /**
  * hash_table_create - function that creates a hash table
  * @size: size of the array
+ *
  * Return: a pointer to the newly created hash table or NULL for error
  */
 hash_table_t *hash_table_create(unsigned long int size)
@@ -12,13 +13,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
-		return NULL;
+		return (NULL);
 
 	table->size = size;
 	table->array = calloc(size, sizeof(hash_table_t));
 
 	if (table->array == NULL)
-		return NULL;
+		return (NULL);
 
 	return (table);
 }
