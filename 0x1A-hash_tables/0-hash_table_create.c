6213dce4118b1149f4682bdaaf27bba7f6ae3fe0
ariana124 <ariana.bibiano@gmail.com>
2020-01-18 22:32:02 +0000

    calloc now allocates for hash_node_t

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,10 +16,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_table_t));
+	table->array = calloc(size, sizeof(hash_node_t));
 
 	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
 
 	return (table);
 }
