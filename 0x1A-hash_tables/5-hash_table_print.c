11e42b8a898bb0664f7f2a72ba8168035b518326
ariana124 <ariana.bibiano@gmail.com>
2020-01-19 02:39:40 +0000

    first commit

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: hash table
+ *
+ * Return: hash table or NULL if hash table is empty
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	int i, flag, size;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
+
+	size = (int)ht->size;
+
+	printf("{");
+
+	flag = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->key);
+			flag = 1;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
