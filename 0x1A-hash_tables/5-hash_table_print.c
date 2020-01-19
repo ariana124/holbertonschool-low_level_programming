3aab7ddef09a77a179cf2e4f1f8f4ef5831971be
ariana124 <ariana.bibiano@gmail.com>
2020-01-19 02:54:31 +0000

    added check for if ht->array is NULL

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,7 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 	int i, flag, size;
 	hash_node_t *node;
 
-	if (ht == NULL)
+	if (ht == NULL || ht->array == NULL)
 		return;
 
 	size = (int)ht->size;
