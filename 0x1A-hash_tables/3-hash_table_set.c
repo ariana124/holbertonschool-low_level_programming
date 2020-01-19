62fa5487fef6cd1389595ef8d59fa523a75df42e
ariana124 <ariana.bibiano@gmail.com>
2020-01-19 00:11:10 +0000

    reset temp to start to connect newNode to table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,7 +35,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = newNode;
 		return (1);
 	}
-
 	temp = ht->array[index];
 	while (temp != NULL)
 	{
@@ -47,7 +46,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		temp = temp->next;
 	}
-
+	temp = ht->array[index];
+	newNode->next = temp;
 	ht->array[index] = newNode;
 	return (1);
 }
