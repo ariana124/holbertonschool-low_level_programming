a15582969f49fc28a471b0ab755af7bfa513b0b5
ariana124 <ariana.bibiano@gmail.com>
2020-04-15 19:03:53 +0000

    header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,15 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int linear_search(int *array, size_t size, int value);
+int binarySearch(int *arr, int left, int right, int value);
+void print_array(int *arr, int left, int right);
+int binary_search(int *array, size_t size, int value);
+
+int jump_search(int *arr, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
