10e5cb4fd7f636aa65b74a7481a4cdb7914c10b5
ariana124 <ariana.bibiano@gmail.com>
2020-04-15 21:17:18 +0000

    updated header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,12 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+
 
 int linear_search(int *array, size_t size, int value);
 int binarySearch(int *arr, int left, int right, int value);
 void print_array(int *arr, int left, int right);
 int binary_search(int *array, size_t size, int value);
-
+int findMin(int a, int b);
 int jump_search(int *arr, size_t size, int value);
+
 
 #endif /* SEARCH_ALGOS_H */
